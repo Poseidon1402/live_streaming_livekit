41ec2290979162c89185d2599a0dc3049fa5852d
Aina Tiavina <82655694+Poseidon1402@users.noreply.github.com>
2025-12-10 18:55:22 +0300

    home: add HomeScreen with LiveKit connection
    - Create `HomeScreen` widget using `CupertinoPageScaffold`.
    - Initialize LiveKit `Room` with `adaptiveStream` and `dynacast` enabled.
    - Implement `initState` to connect to the room using hardcoded credentials, enable camera/microphone, and create a `LocalVideoTrack`.
    - Add UI to render the local video feed using `VideoTrackRenderer` or display a loading indicator.
    - Ensure room and track resources are released in `dispose`.

---
+++ lib/home_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:livekit_client/livekit_client.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final roomOptions = RoomOptions(adaptiveStream: true, dynacast: true);
+  LocalVideoTrack? localVideoTrack;
+  static const url = "wss://livestream-q5sk4tqa.livekit.cloud";
+  static const token =
+      "eyJhbGciOiJIUzI1NiJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6InF1aWNrc3RhcnQtcm9vbSJ9LCJpc3MiOiJBUElqRERTNUJ0azNLaksiLCJleHAiOjE3NjUzODY2MTcsIm5iZiI6MCwic3ViIjoicXVpY2tzdGFydC11c2VybmFtZSJ9.3dDRkZpWjKhlsiEfSnlOPEABHh8UWSNe5uA-dxUIqeM";
+
+  final room = Room();
+
+  @override
+  void initState() {
+    room
+        .connect(url, token, roomOptions: roomOptions)
+        .then((_) async {
+          await room.localParticipant?.setCameraEnabled(true);
+          await room.localParticipant?.setMicrophoneEnabled(true);
+          LocalVideoTrack.createCameraTrack().then(
+            (track) => setState(() => localVideoTrack = track),
+          );
+          // await room.localParticipant?.publishTrack(localVideoTrack);
+        })
+        .catchError((error) {
+          // Handle connection error
+          debugPrint('Error connecting to room: $error');
+        });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // Release resources
+    room.dispose();
+    localVideoTrack?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(middle: Text('Home Screen')),
+      child: Visibility(
+        visible: localVideoTrack != null,
+        child: localVideoTrack != null
+            ? VideoTrackRenderer(localVideoTrack!, fit: VideoViewFit.cover)
+            : Center(child: CupertinoActivityIndicator()),
+      ),
+    );
+  }
+}
