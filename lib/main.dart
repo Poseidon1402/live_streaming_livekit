94719591189771bf06b387d753502d34adcc716f
Aina Tiavina <82655694+Poseidon1402@users.noreply.github.com>
2025-12-13 17:33:46 +0300

    app: setup AuthBloc and implement named routing
    - Wrap `MaterialApp` with `BlocProvider` to initialize `AuthBloc`.
    - Create `Routes` class in `lib/utils/constants/routes.dart` with login and home constants.
    - Configure `MaterialApp` to use `initialRoute` and define the `routes` map for `LoginScreen` and `HomeScreen`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'bloc/auth/auth_bloc.dart';
+import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
+import 'utils/constants/routes.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Live stream',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return BlocProvider(
+      create: (_) => AuthBloc(),
+      child: MaterialApp(
+        initialRoute: Routes.login,
+        title: 'Live stream',
+        debugShowCheckedModeBanner: false,
+        routes: {
+          Routes.login: (context) => const LoginScreen(),
+          Routes.home: (context) => const HomeScreen(),
+        },
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
       ),
-      home: const LoginScreen(),
     );
   }
 }
