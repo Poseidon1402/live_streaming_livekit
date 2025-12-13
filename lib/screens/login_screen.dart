0e03f13d2e7f26b458fc432f281880bd90e996ec
Aina Tiavina <82655694+Poseidon1402@users.noreply.github.com>
2025-12-13 17:38:43 +0300

    login: implement login functionality with AuthBloc
    - Convert `LoginScreen` from `StatelessWidget` to `StatefulWidget` to manage text controller and form state.
    - Add `TextEditingController` for username input and handle disposal.
    - Implement UI structure with `BlocListener`, `Form`, `CupertinoTextFormFieldRow` for input, and `CupertinoButton` for submission.
    - Add `AuthBloc` integration to dispatch `LoginRequestedEvent` upon valid form submission.
    - Listen for `AuthenticatedState` to show a success dialog and navigate to `Routes.home`.
    - Include form validation using `isNotEmpty`.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,15 +1,87 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class LoginScreen extends StatelessWidget {
+import '../bloc/auth/auth_bloc.dart';
+import '../utils/constants/routes.dart';
+import '../utils/validator/form_validator.dart';
+
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late final TextEditingController _usernameController;
+
+  @override
+  void initState() {
+    _usernameController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(middle: Text('Login Screen')),
-      child: Center(
-        child: Text('Login Screen Content'),
+      child: Container(
+        constraints: BoxConstraints.expand(
+          height: MediaQuery.sizeOf(context).height,
+        ),
+        child: BlocListener<AuthBloc, AuthState>(
+          listener: (_, state) async {
+            if (state is AuthenticatedState) {
+              showCupertinoDialog(
+                context: context,
+                builder: (context) => CupertinoAlertDialog(
+                  title: const Text('Success'),
+                  content: const Text('You are authenticated successfully!'),
+                  actions: [
+                    CupertinoDialogAction(
+                      child: const Text('OK'),
+                      onPressed: () => Navigator.popAndPushNamed(context, Routes.home),
+                    ),
+                  ],
+                ),
+              );
+            }
+          },
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CupertinoTextFormFieldRow(
+                  controller: _usernameController,
+                  placeholder: 'Enter your username',
+                  validator: isNotEmpty,
+                ),
+                SizedBox(height: 20),
+                CupertinoButton.filled(
+                  onPressed: _onLoginBtnTapped,
+                  child: Text('Login'),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  void _onLoginBtnTapped() {
+    if (_formKey.currentState!.validate()) {
+      // Remove extra spaces
+      final username = _usernameController.text.trim();
+      context.read<AuthBloc>().add(LoginRequestedEvent(username: username));
+    }
   }
 }
