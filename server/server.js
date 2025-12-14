17238919a082f67c171315e81edd9aee8cab0298
Aina Tiavina <82655694+Poseidon1402@users.noreply.github.com>
2025-12-14 15:04:54 +0300

    feat: add CORS support to server and update token creation logic

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 // server.js
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import { AccessToken } from 'livekit-server-sdk';
+
+// Fix CORS issues
 
 const createToken = async () => {
   // If this room doesn't exist, it'll be automatically created when the first
@@ -9,20 +12,24 @@ const createToken = async () => {
   const roomName = 'quickstart-room';
   // Identifier to be used for participant.
   // It's available as LocalParticipant.identity with livekit-client SDK
-  const participantName = 'quickstart-username';
 
   const at = new AccessToken(process.env.LIVEKIT_API_KEY, process.env.LIVEKIT_API_SECRET, {
-    identity: participantName,
+    identity: `${Date.now()}`, // Let JS side assign a random identity using datetime
     // Token to expire after 100 minutes
     ttl: '100m',
   });
-  at.addGrant({ roomJoin: true, room: roomName });
+  at.addGrant({ roomJoin: true, room: roomName, canPublish: true, });
 
   return await at.toJwt();
 };
 
 const app = express();
 const port = 3000;
+
+app.use(cors({
+  origin: '*',  // Allow all origins
+  methods: ['GET', 'POST'],
+}));
 
 app.get('/getToken', async (req, res) => {
   res.send(await createToken());
